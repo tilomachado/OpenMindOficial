c744fc13fb8ec1388704f80bdb8456cc6e48528e
Tilo Machado <estrategiadenegociosprosperos@gmail.com>
2025-02-23 14:22:44 +0200

    Add files via upload

--- cursos/script.js
+++ cursos/script.js
@@ -1,4 +1,4 @@
 // Redirecionamento para a página de pagamento
 document.getElementById('inscrevaBtn').addEventListener('click', function() {
-    window.location.href = 'pagamento.html';
+    window.location.href = 'entrar.html';
 });
